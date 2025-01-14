094a746a955c927b20a62d104011c168f5044301
adamtokola <adamtokola@gmail.com>
2025-01-14 18:52:33 -0500

    added components
     components to fetch books

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,31 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function App() {
-    const [message, setMessage] = useState('');
+  const [books, setBooks] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:5001')
-            .then((response) => response.text())
-            .then((data) => setMessage(data))
-            .catch((error) => console.error('Error:', error));
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:5001/books")
+      .then((response) => response.json())
+      .then((data) => setBooks(data))
+      .catch((error) => console.error("Error:", error));
+  }, []);
 
-    return (
-        <div>
-            <h1>Book Haven</h1>
-            <p>{message}</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Book Haven</h1>
+      <h2>Book List</h2>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>
+            <h3>{book.title}</h3>
+            <p>Author: {book.author}</p>
+            <p>Genre: {book.genre}</p>
+            <p>Rating: {book.average_rating}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
