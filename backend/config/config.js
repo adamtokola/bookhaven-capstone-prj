9fb15a794061e87028cec535b9e29940f9327f09
adamtokola <adamtokola@gmail.com>
2025-01-14 23:49:00 -0500

    updates for database
    add starters for seeders and migrations

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,16 +1,26 @@
-const { Sequelize } = require('sequelize');
 const dotenv = require('dotenv');
-
 dotenv.config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: 'postgres',
-    }
-);
-
-module.exports = sequelize;
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  test: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+};
